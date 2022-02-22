0f331d6e6e3218b83cfef2d370845271a2f87e34
Erdnaxela3 <w.alexandre2001@hotmail.fr>
2022-02-22 04:02:57 +0100

    ca marche

--- canny.py
+++ canny.py
@@ -5,7 +5,7 @@ from matplotlib import image
 from skimage import data,io,filters,draw
 import time
 from scipy.ndimage import convolve
-from math import sqrt,atan2,pi
+from math import sqrt,atan2,pi,cos,sin
 
 #imagegris
 def to_gray(im):
@@ -15,51 +15,57 @@ def to_gray(im):
     Return:
         retourne le np.array (2 dim 1 canal) de l'image donnée en paramètre en niveau de gris
     """
-    return ((im[:,:,0]+im[:,:,1]+im[:,:,2])*85).astype("uint8")
+    return ((im[:,:,0]+im[:,:,1]+im[:,:,2])/3).astype("uint8")
 
 #flou gaussien
-def to_blur(im):
+def to_blur(im,n):
     # Gaussian kernel
     kernel = np.outer([1,4,6,4,1],[1,4,6,4,1])/256
-    return convolve(im,kernel)
+    
+    res = convolve(im,kernel)
+    
+    for i in range(1,n):
+        res = convolve(res,kernel)
+        
+    return res
 
 #gradient
 #https://fr.wikipedia.org/wiki/Filtre_de_Canny
 def gradient(im):
-
+    
     width = im.shape[1]
     height = im.shape[0]
-
+    
     zerosVert = np.zeros((height,1),dtype = "uint8")
     zerosHoriz = np.zeros((1,width),dtype = "uint8")
     pixeldroite = np.concatenate((im[:,1:],zerosVert),axis = 1)
     pixelgauche = np.concatenate((zerosVert,im[:,:-1]),axis = 1)
     pixelhaut = np.concatenate((zerosHoriz,im[:-1,:]),axis = 0)
     pixelbas = np.concatenate((im[1:,:],zerosHoriz),axis = 0)
-
+    
     gradientx = pixeldroite - pixelgauche
     gradientx[:,(0,-1)],gradientx[(0,-1),:] = 0,0
-
+    
     gradienty = pixelhaut - pixelbas
     gradienty[(0,-1),:],gradienty[:,(0,-1)] = 0,0
-
+    
     gradient = np.sqrt(gradientx**2 + gradienty**2)
     direction = np.arctan2(gradienty,gradientx)
-
+    
     return gradient,direction#,gradientx,gradienty,pixelhaut,pixelbas,pixeldroite,pixelgauche
 
 #filtrage des non max
 def filter_non_max(grad, direction):
     width = grad.shape[1]
     height = grad.shape[0]
-
+    
     angle = np.where(direction >= 0,direction,direction+pi)
     rangle = np.round(angle/(pi/4))
     mag = grad
-
+    
     zerosVert = np.zeros((height,1),dtype = "uint8")
     zerosHoriz = np.zeros((1,width),dtype = "uint8")
-
+    
     pixeldroite = np.concatenate((grad[:,1:],zerosVert),axis = 1)
     pixelgauche = np.concatenate((zerosVert,grad[:,:-1]),axis = 1)
     pixelhaut = np.concatenate((zerosHoriz,grad[:-1,:]),axis = 0)
@@ -68,7 +74,7 @@ def filter_non_max(grad, direction):
     pixelhautdroite = np.concatenate((zerosHoriz,pixeldroite[:-1,:]),axis = 0)
     pixelbasgauche = np.concatenate((pixelgauche[1:,:],zerosHoriz),axis = 0)
     pixelbasdroite = np.concatenate((pixeldroite[1:,:],zerosHoriz),axis = 0)
-
+    
     rangle0or4 = np.logical_or(rangle == 0, rangle == 4)
     gauchedroitemag = np.logical_or(pixelgauche > mag, pixeldroite > mag)
 
@@ -76,55 +82,71 @@ def filter_non_max(grad, direction):
     cond2 = np.logical_and(rangle == 1, np.logical_or(pixelhautgauche>mag, pixelbasdroite > mag))
     cond3 = np.logical_and(rangle == 2, np.logical_or(pixelhaut>mag, pixelbas > mag))
     cond4 = np.logical_and(rangle == 3, np.logical_or(pixelhautdroite>mag, pixelbasgauche > mag))
-
+    
     cond = np.logical_or.reduce((cond1,cond2,cond3,cond4))
-
+    
     grad = np.where(cond, 0, grad)
 
-
+    
     return grad
 
 #filtrage bord fort
 def filter_strong_edges(grad, low, high):
-
+    
     #on garde les bords forts dans keep
-    x,y = np.indices(grad.shape)
-    keep = np.stack((x,y),axis=2)[grad>high]
+    keep = grad > high
+    
+    lastNbrOfTrue = -1
+    
+    while(lastNbrOfTrue != np.sum(keep)):
+        
+        lastNbrOfTrue = np.sum(keep)
+        
+        #on garde les bords faibles autour de bords à garder
 
-    #on garde les bords faibles autour de bords à garder
-    foundnew = True
-    while foundnew:
-        for t in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-            newpos = keep + t
-            newkeep = newpos[grad[newpos[:,0],newpos[:,1]]>low]
-        foundnew = np.setdiff1d(newkeep,keep).shape[0] != 0
-        print(len(np.setdiff1d(newkeep,keep)))
-        #foundnew = False
-        keep = np.concatenate((keep, newkeep), axis = 0)
-        keep = np.unique(keep, axis = 0)
+        zerosVert = np.zeros((grad.shape[0],1),dtype = "uint8")
+        zerosHoriz = np.zeros((1,grad.shape[1]),dtype = "uint8")
 
+        pixeldroite = np.concatenate((keep[:,1:],zerosVert),axis = 1)
+        pixelgauche = np.concatenate((zerosVert,keep[:,:-1]),axis = 1)
+        pixelhaut = np.concatenate((zerosHoriz,keep[:-1,:]),axis = 0)
+        pixelbas = np.concatenate((keep[1:,:],zerosHoriz),axis = 0)
+        pixelhautgauche = np.concatenate((zerosHoriz,pixelgauche[:-1,:]),axis = 0)
+        pixelhautdroite = np.concatenate((zerosHoriz,pixeldroite[:-1,:]),axis = 0)
+        pixelbasgauche = np.concatenate((pixelgauche[1:,:],zerosHoriz),axis = 0)
+        pixelbasdroite = np.concatenate((pixeldroite[1:,:],zerosHoriz),axis = 0)
+
+        #si un des pixels autour est un bord à garder
+        cond = np.logical_or.reduce((pixelbas,pixelhaut,pixeldroite,pixelgauche,pixelhautgauche,pixelhautdroite,pixelbasdroite,pixelbasgauche))
+        cond = np.logical_and(grad>low,cond) #si le pixel satisfait le seuil low et q'un pixel autour est à garder
+
+        #agglomération des nouveaux bords
+        keep = np.logical_or(cond,keep)
+        
     return keep
 
-def canny_edge_detector(im, low, high,blurfactor):
+def resolutiondown(im,maxsize):
+    """reduit la resolution de l'image pour avoir une hauteur/largeur maximale de maxsize"""
+    div_factor = max(im.shape)//maxsize
+    return im[::div_factor, ::div_factor]
+
+def canny_edge_detector(im, low, high, blurfactor):
     width = im.shape[1]
     height = im.shape[0]
-
+    
     #gris
     grayim = to_gray(im)
-
+    
     #blur
-    blurim = to_blur(grayim)
-    for i in range(blurfactor):
-        blurim = to_blur(blurim)
-
-
+    blurim = to_blur(grayim, blurfactor)
+    
     #gradient
     grad, direction = gradient(blurim)
-
+    
     #non max
     grad = filter_non_max(grad, direction)
-
+    
     #filter edges
-    keep = filter_strong_edges(grad,low,high)
-
+    keep = filter_strong_edges(grad, low, high)
+    
     return keep
